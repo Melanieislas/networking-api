a04cb8e2c6f2a658190c62d5a1d63a8e00e131dc
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-06-01 23:10:42 -0700

    added user and thought schemas, set up and installed mongoose and other dependencies

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+import { isEmail } from 'validator';
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        }
+    },
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [ isEmail, 'invalid email' ],
+        }
+    },
+    {
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+    },
+    {
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function() {
+        return this.friends.length;
+    });
+
+    const User = model('user', userSchema);
+
+module.exports = User;
